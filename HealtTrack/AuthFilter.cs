47d28eb6cfc8e1ca25c9896cf3aee9f077d25c8d
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2023-11-14 21:12:32 -0300

    Creat HealthTrack AspNetCore

---
+++ HealtTrack/AuthFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+public class AuthFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // Verifique se o usuário está autenticado.
+        if (context.HttpContext.User.Identity.IsAuthenticated == false)
+        {
+            // Redirecione o usuário para a página de login.
+            context.Result = new RedirectResult("/Login");
+        }
+    }
+}
---
+++ HealtTrack/Program.cs
@@ -0,0 +1,54 @@
+using HealtTrack.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>{
+
+        options.AccessDeniedPath = "/Usuario/AccessDenied/";
+        options.LoginPath = "/Usuarios";
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Usuarios}/{action=Login}/{id?}");
+
+app.Run();
